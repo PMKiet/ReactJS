4e440a36494e9838b84026d83ae0b31e6c7f95a2
Pham Minh Kiet <minhkiet3466@gmail.com>
2023-09-10 14:37:40 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -2,55 +2,14 @@ import { useState } from 'react';
 import './App.css';
 
 
-const courses = [
-  {
-    id: 1,
-    name: "Html, Css",
-  },
-  {
-    id: 2,
-    name: "ReactJs",
-  },
-  {
-    id: 3,
-    name: "PHP",
-  },
-]
-
 function App() {
 
-  //input
-  // const [name, setName] = useState('')
-  // const [email, setEmail] = useState('')
-
-  // function handleSubmit() {
-  //   console.log(
-  //     {
-  //       name: name,
-  //       email: email
-  //     }
-  //   );
-  // }
-
-
-  const [check, setCheck] = useState(1);
 
 
   return (
 
     <div className="App">
-        {
-          courses.map(course => (
-            <div key={course.id}>
-              <input 
-                type='radio'
-                checked={check === course.id}
-                onChange={() => setCheck(course.id)}
-              />
-              {course.name}
-            </div>
-          ))
-        }
+       <h1>Phạm Minh Kiệt</h1>
     </div>
   );
 
